50c45d9c0aa42f9e66ad27ccd61f7e17ed256b63
piansaen <pian.python@gmail.com>
2022-12-08 08:28:11 +0700

    Create pretest.py

---
+++ pretest.py
@@ -0,0 +1,23 @@
+#name = input("whai is your name: ")
+#print ("your name is :" + name)
+
+adj = input("adjective : ")
+verb1 = input("verb1 : ")
+verb2 = input("verb2 : ")
+famous_person = input("Famous person :")
+
+malib = f"computer {adj} it make me so excited all the time because \
+i live to {verb1} . stay hydrated and {verb2} like you are {famous_person} "
+
+print(malib)
+
+oun = f"sdfsdfsdfsdd {adj}"
+
+print("name :"+oun)
+
+
+
+
+
+
+
